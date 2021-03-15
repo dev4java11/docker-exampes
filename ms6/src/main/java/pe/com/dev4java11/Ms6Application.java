83036307c718d101236b5af1d40f686388e3ebb3
dev4java11 <dev4java11@gmail.com>
2021-03-15 18:29:34 -0500

    updated ms6.

--- ms6/src/main/java/pe/com/dev4java11/Ms6Application.java
+++ ms6/src/main/java/pe/com/dev4java11/Ms6Application.java
@@ -1,11 +1,13 @@
 package pe.com.dev4java11;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,9 +20,24 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @Slf4j
 public class Ms6Application {
+	
+	public static final Double SALARY_2000 = Double.valueOf("2000");
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ms6Application.class, args);
+	}
+	
+	@Autowired
+	private StreamBridge streaming;
+	
+	@Bean
+	public Consumer<Employee> filterGreaterEqualSalary2000() {
+		return e -> {
+			if(e.getSalary().compareTo(SALARY_2000) >= 0) {
+				log.info("employee.salary >= 2000: {}", e);
+				streaming.send("filterGreaterEqualSalary2000-out-0", e);
+			}
+		};
 	}
 	
 	@RestController
@@ -32,8 +49,8 @@ public class Ms6Application {
 		
 		@PostMapping("/ingest/single")
 		public ResponseEntity<?> ingest(@RequestBody Employee person){
-			log.info("ingest data: {}", person);
-			bridge.send("ingest-out-0", person);
+			log.info("ingest employee: {}", person);
+			bridge.send("employee-out-0", person);
 			return ResponseEntity.ok("");
 		}
 		
